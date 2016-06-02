0dffbc9c3aa74299a55fc26bd18876dae1a64876
Hideki Abe <hideki.abe.0630@gmail.com>
2016-06-02 16:08:31 +0900

    カテゴリIDの指定に対応

--- plugins/ArchivedEntries/lib/ArchivedEntries/Tags.pm
+++ plugins/ArchivedEntries/lib/ArchivedEntries/Tags.pm
@@ -1,6 +1,7 @@
 package ArchivedEntries::Tags;
 use strict;
 use MT::Entry;
+use MT::Placement;
 
 sub _hdlr_archived_entries {
     my ($ctx, $args, $cond) = @_;
@@ -12,16 +13,37 @@ sub _hdlr_archived_entries {
     my $start = sprintf "%4d%02d01000000", $year, $month;
     my $end   = sprintf "%4d%02d%02d235959", $year, $month, $day - 1;
     my $blog = $ctx->stash('blog');
-    my @entries = MT::Entry->load(
-        {   blog_id     => $blog->id,
-            status      => MT::Entry::RELEASE(),
-            authored_on => [ $start, $end ],
-        },
-        {   range_incl  => { authored_on => 1 },
-            'sort'      => 'authored_on',
-            'direction' => 'descend',
-        }
-    ) or return $ctx->error("Couldn't get entry list");
+    my $category_id = $args->{ category_id } || '';
+    my @entries;
+
+    if ($category_id) {
+        @entries = MT::Entry->load(
+            {   blog_id     => $blog->id,
+                status      => MT::Entry::RELEASE(),
+                authored_on => [ $start, $end ],
+            },
+            {   range_incl  => { authored_on => 1 },
+                'sort'      => 'authored_on',
+                'direction' => 'descend',
+                join        => MT::Placement->join_on(
+                    'entry_id',
+                    { category_id => $category_id },
+                    { unique => 1 }
+                )
+            }
+        ) or return $ctx->error("Couldn't get entry list");
+    } else {
+        @entries = MT::Entry->load(
+            {   blog_id     => $blog->id,
+                status      => MT::Entry::RELEASE(),
+                authored_on => [ $start, $end ],
+            },
+            {   range_incl  => { authored_on => 1 },
+                'sort'      => 'authored_on',
+                'direction' => 'descend',
+            }
+        ) or return $ctx->error("Couldn't get entry list");
+    }
 
     local $ctx->{__stash}{entries} = \@entries;
     return $ctx->invoke_handler('entries', $args, $cond);
