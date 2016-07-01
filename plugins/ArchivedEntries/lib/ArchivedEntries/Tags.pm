476110d8eb6a12c6c11db7e8def8bbc6d0732f39
Hideki Abe <hideki.abe.0630@gmail.com>
2016-07-01 11:36:28 +0900

    月初で記事がない場合の挙動を修正

--- plugins/ArchivedEntries/lib/ArchivedEntries/Tags.pm
+++ plugins/ArchivedEntries/lib/ArchivedEntries/Tags.pm
@@ -10,32 +10,35 @@ sub _hdlr_archived_entries {
     $year += 1900;
     $month += 1;
 
-    my $start = sprintf "%4d%02d01000000", $year, $month;
-    my $end   = sprintf "%4d%02d%02d235959", $year, $month, $day - 1;
-    my $blog = $ctx->stash('blog');
-    my $category_id = $args->{ category_id } || '';
-    my @entries;
-    my %load_args;
+    my @entries = ();
 
-    if ($category_id) {
-        $load_args{ join } = MT::Placement->join_on(
-                                    'entry_id',
-                                    { category_id => $category_id },
-                                    { unique => 1 }
-                                );
+    if ($day > 1) {
+        my $start = sprintf "%4d%02d01000000", $year, $month;
+        my $end   = sprintf "%4d%02d%02d235959", $year, $month, $day - 1;
+        my $blog = $ctx->stash('blog');
+        my $category_id = $args->{ category_id } || '';
+        my %load_args;
+
+        if ($category_id) {
+            $load_args{ join } = MT::Placement->join_on(
+                                        'entry_id',
+                                        { category_id => $category_id },
+                                        { unique => 1 }
+                                    );
+        }
+
+        $load_args{ range_incl } = { authored_on => 1 };
+        $load_args{ sort } = 'authored_on';
+        $load_args{ direction } = 'descend';
+
+        @entries = MT::Entry->load(
+            {   blog_id     => $blog->id,
+                status      => MT::Entry::RELEASE(),
+                authored_on => [ $start, $end ],
+            },
+            \%load_args
+        );
     }
-
-    $load_args{ range_incl } = { authored_on => 1 };
-    $load_args{ sort } = 'authored_on';
-    $load_args{ direction } = 'descend';
-
-    @entries = MT::Entry->load(
-        {   blog_id     => $blog->id,
-            status      => MT::Entry::RELEASE(),
-            authored_on => [ $start, $end ],
-        },
-        \%load_args
-    ) or return $ctx->error("Couldn't get entry list");
 
     local $ctx->{__stash}{entries} = \@entries;
     return $ctx->invoke_handler('entries', $args, $cond);
